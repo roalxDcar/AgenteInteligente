5d7b664815776a247ed6a3f27a66eed95b39bbd1
roalxDcar <roalmollericona@gmail.com>
2021-06-07 18:45:28 -0400

    first commit

---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+	public Transform Player;
+	// Velocidad de Translación
+	public float velocidad;
+	// Velocidad de Giro
+	public float velocidadGiro;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+        	Player.Translate(Vector3.forward * velocidad);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+        	Player.Translate(Vector3.back * velocidad);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+        	Player.Rotate(Vector3.down * velocidadGiro);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+        	Player.Rotate(Vector3.up * velocidadGiro);
+        }
+    }
+}
---
+++ Assets/NavMeshController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Apliación de Inteligenecia Artifial
+using UnityEngine.AI;
+
+public class NavMeshController : MonoBehaviour
+{
+	public Transform objetivo;
+	private NavMeshAgent agente;
+    // Se llama al inicio antes de la primera actualización del cuadro
+    void Start()
+    {
+    	// Instanacia de agente
+        agente = GetComponent<NavMeshAgent>();
+        // instanciar componente para el movimiento por teclado
+    }
+
+    // La actualización se llama una vez por fotograma
+    void Update()
+    {
+    	//Nuestro agente tendra como destino a objetivo(esfera)
+        agente.destination = objetivo.position;
+    }
+}
